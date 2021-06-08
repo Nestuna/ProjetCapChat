9e692e50f03bdbe350282684739d15a3a607835f
nantunes <nicolas.antunes@ubicast.eu>
2021-06-08 16:22:08 +0200

    add style and countdown

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
+import homeRoute from './routes/home.js'
+
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+app.set('view engine', 'ejs')
 app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(express.json())
-import homeRoute from './routes/home.js'
 
 app.use('/', homeRoute)
 
--- routes/home.js
+++ routes/home.js
@@ -1,24 +1,33 @@
+import { log } from "console"
 import express from "express"
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import CaptchaHandler from "../controllers/CaptchaHandler.js"
+import ImagesGenerator from "../controllers/CaptchaHandler.js"
 
-import ImagesHandler from '../controllers/ImagesHandler.js'
+import ImagesHandler from '../controllers/CaptchaHandler.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const router = express.Router()
-const img = new ImagesHandler()
+const captcha = new CaptchaHandler()
 
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/index.html'))
+    let attempts = 0
+    if (req.query['attempts']) {
+        attempts = req.query['attempts']
+    }
+    const {neutralImagesPaths, singularImagePath, clue } = captcha.getImagesAndClue()
+    const data = {
+        neutralImagesPaths: neutralImagesPaths,
+        singularImagePath: singularImagePath,
+        clue: clue,
+        attempts: attempts
+    }
+    console.log(data)
+    res.render('index', data)
 })
 
-router.get('/images', (req, res) => {
-    const neutralImages = img.getRandomNeutralImages()
-    const singularImage = img.getRandomSingularImage()
-    console.log(`Neutral Images selected : ${neutralImages.map(img => path.basename(img))}`);
-    console.log(`Singular Image selected : ${path.basename(singularImage)}`);
-})
 
 export default router
